263ea7f978d4ac5c17eaa2762e68a90766211edb
Radnyx <radnyx.again@gmail.com>
2024-01-29 18:03:47 -0800

    setup window interface

--- native/main.cpp
+++ native/main.cpp
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <cstdint>
+#include "window.h"
 
 #ifdef _WIN32
 #define DLLEXPORT __declspec(dllexport)
@@ -13,32 +14,3 @@
 #endif
 
 extern DLLEXPORT int main();
-
-struct Window;
-
-struct Kernel { 
-    const uint8_t * code;
-    size_t size;
-};
-
-Window * createWindow(const char * title, uint32_t width, uint32_t height) {
-    // TODO
-    return nullptr;
-}
-
-void createDemoPipeline(Window * window, Kernel kernel) {
-    // TODO
-}
-
-void update(Window * window) {
-    // TODO
-}
-
-bool closed(Window * window) {
-    // TODO
-    return true;
-}
-
-void destroyWindow(Window * window) {
-    // TODO
-}
---
+++ native/window.h
@@ -0,0 +1,110 @@
+#ifndef _WINDOW_H_
+#define _WINDOW_H_
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+#include <optional>
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+
+    bool isComplete();
+};
+
+struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
+
+class VulkanApplication {
+public:
+    VulkanApplication(const char * title, uint32_t width, uint32_t height);
+    ~VulkanApplication();
+    void update();
+    bool closed();
+private:
+    GLFWwindow * window;
+
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
+    VkSurfaceKHR surface;
+
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+
+    VkQueue graphicsQueue;
+    VkQueue presentQueue;
+
+    VkSwapchainKHR swapChain;
+    std::vector<VkImage> swapChainImages;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
+    std::vector<VkImageView> swapChainImageViews;
+    std::vector<VkFramebuffer> swapChainFramebuffers;
+
+    VkRenderPass renderPass;
+    VkPipelineLayout pipelineLayout;
+    VkPipeline graphicsPipeline;
+
+    VkCommandPool commandPool;
+    VkCommandBuffer commandBuffer;
+
+    VkSemaphore imageAvailableSemaphore;
+    VkSemaphore renderFinishedSemaphore;
+    VkFence inFlightFence;
+
+    void initVulkan();
+    void drawFrame();
+
+    void createInstance();
+    void createSurface();
+    void pickPhysicalDevice();
+    void createLogicalDevice();
+    void createSwapChain();
+    void createImageViews();
+    void createRenderPass();
+    void createGraphicsPipeline();
+    void createFramebuffers();
+    void createCommandPool();
+    void createCommandBuffer();
+    void createSyncObjects();
+
+    void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+    VkShaderModule createShaderModule(const std::vector<char>& code);
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+    bool isDeviceSuitable(VkPhysicalDevice device);
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+    std::vector<const char*> getRequiredExtensions();
+    bool checkValidationLayerSupport();
+};
+
+struct Window final {
+    std::unique_ptr<VulkanApplication> app;
+    bool forceQuit;
+};
+
+/*
+struct Kernel { 
+    const uint8_t * code;
+    size_t size;
+};
+*/
+
+Window * createWindow(const char * title, uint32_t width, uint32_t height);
+
+// void createDemoPipeline(Window * window, Kernel kernel);
+
+void update(Window * window);
+
+bool closed(Window * window);
+
+void destroyWindow(Window * window);
+
+#endif // _WINDOW_H_
